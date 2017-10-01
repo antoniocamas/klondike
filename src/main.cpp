2385ab3cf6c71edb121532291147dfecbe3da8ed
Antonio Camas <antoniocamas@hotmail.com>
2017-10-01 15:23:16 +0200

    Initial commit. Dummy main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 1, 2017
+ *      Author: antonio
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+	cout << "Clondike Project" << endl;
+	vector<int> test = {1,2};
+
+	return 0;
+}
+
+
